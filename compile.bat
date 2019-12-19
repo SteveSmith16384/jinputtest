bc617505e46608ad71a36e494e78d45a049c1e77
Stephen Carlyle-Smith <stephen.carlylesmith@googlemail.com>
2019-12-19 09:22:27 +0000

    Initial checkin

---
+++ compile.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\Java\jdk1.8.0_151\bin\javac.exe" -cp libs\jinput-2.0.9.jar;libs\jinput-2.0.9-natives-all.jar -sourcepath src src\jinputtest\ReadAllEvents.java
---
+++ run.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\Java\jdk1.8.0_151\bin\java.exe" -cp libs\jinput-2.0.9.jar;libs\jinput-2.0.9-natives-all.jar;bin jinputtest.ReadAllEvents
